deed0865ef39e9c4a2a2fa24ddcbd6521636ef86
Noart1184 <noart.ramadani@students.askosova.org>
2025-01-25 07:45:25 -0800

    first php file

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+// $name = "Noart";
+
+// echo "I dont like school"
+// $age = 22;
+
+
+
+
+// $x = 120;
+// $y = 20;
+
+// echo $x * $y
+
+$name = "Noart";
+$surname = "Ramadani";
+
+$fullName = $name . " " . $surname;
+
+echo $fullName;
+echo strlen($name);
+echo str_word_count($name);
+echo strrev($name);
+
+
+
+?>
