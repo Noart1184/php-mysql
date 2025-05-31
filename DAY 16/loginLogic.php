ea989c673cabaa96c1da15994e32d0969c8d0b86
Noart1184 <noart.ramadani@students.askosova.org>
2025-05-31 07:44:38 -0700

    a

---
+++ DAY 16/loginLogic.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+include_once('config.php');
+
+if(isses($_POST['submit'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    if (empty($username) || empty($password)) {
+        echo "Please fill in all fields";
+}
